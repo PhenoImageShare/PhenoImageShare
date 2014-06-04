3ac43067373f6dc505b600ffce3937e817e129b6
David Osumi-Sutherland <dosumis@gmail.com>
2014-06-04 12:17:09 +0100

    editing gen bindings to work with java libs on server. Temporarily disable pyxb gen bindings for testing in absence of rqd python package on server.

--- VFB_import/build/gen_bindings.sh
+++ VFB_import/build/gen_bindings.sh
@@ -1,5 +1,9 @@
 # Generate pretty svg for doc purposes
-java -jar ${JLPATH}/xsdvi.jar ../../PhiS/source/main/resources/phisSchema.xsd
+java -jar ../lib/xsdvi.jar ../../PhiS/source/main/resources/phisSchema.xsd
+# update file for easy viewing of svg and for recording date.
+
 # generate pyxb python data model lib
-pyxbgen \
-   -u ../../PhiS/source/main/resources/phisSchema.xsd -m phisSchema
+#pyxbgen \
+#   -u ../../PhiS/source/main/resources/phisSchema.xsd -m phisSchema
+
+
