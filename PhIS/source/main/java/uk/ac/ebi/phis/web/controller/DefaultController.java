163b7dc6574f9b6d4eb3103fdf6a8c4dba2c1fde
Ilinca Tudose <ilinca.tudose@gmail.com>
2014-07-25 17:16:17 +0100

    removed /*

--- PhIS/source/main/java/uk/ac/ebi/phis/web/controller/DefaultController.java
+++ PhIS/source/main/java/uk/ac/ebi/phis/web/controller/DefaultController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DefaultController {
 
-	@RequestMapping(value="/*", method=RequestMethod.GET)	
+	@RequestMapping(value="/", method=RequestMethod.GET)	
     public String showSomething( Model model ) throws SolrServerException, IOException, URISyntaxException {
 	
 		System.out.println("Try /getImages .");
