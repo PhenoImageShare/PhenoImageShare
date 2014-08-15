6779bae71f867bc8c9165de0ac202e6b16c9f5e1
Kenneth McLeod <kcm1@hw.ac.uk>
2014-08-15 14:40:06 +0100

    Added first (very basic) version of IQS.  Only provides access to getChannel, GetRoi & getImages. Includes documentation in index.jsp.  Very basic logging specified in logging.properties.

---
+++ HWU/src/main/java/uk/ac/hw/macs/bisel/phis/iqs/GetImages.java
@@ -0,0 +1,212 @@
+package uk.ac.hw.macs.bisel.phis.iqs;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet connects client (presumably a GUI) to the SOLR API that wraps the SOLR repository
+ * for images
+ *
+ * @author kcm
+ */
+//@WebServlet(name = "GetImages", urlPatterns = {"/GetImages"})
+public class GetImages extends HttpServlet {
+
+    private static final String url = "http://dev.phenoimageshare.org/data/rest/getImages?"; // stem of every SOLR query
+    private static final Logger logger = Logger.getLogger(System.class.getName());
+    /**
+     * Enables discovery of images that meet the user's requirements.
+     * Handles requests from the PhIS UI by simply forwarding them to the SOLR API and 
+     * then returning the result directly to the UI.  Provides very basic error handling
+     * (only deals with unknown parameters).
+     *
+     * Queries the SOLR API, not the SOLR core.  Use the pagination facilities offered by the SOLR API
+     * 
+     * Parameters expected:
+     * <ol>
+     * <li>ptId = phenotype ID, i.e., a MP ID</li>
+     * <li>aId = EMAPA or MA ID</li>
+     * <li>gId = MGI gene ID, e.g., MGI:12344567</li>
+     * <li>start = pagination, the first result that should be returned>/li>
+     * <li>num = pagination, the total number of results that should be returned</li>
+     * </ol>
+     * 
+     * Future versions will:
+     * <ol>
+     * <li>send queries to the SIS, and then integrate the results
+     * with those from SOLR</li>  
+     * <li>likely to include sorting the results</li>
+     * <li>include a wider range of query parameters</li>
+     * <li>provide access to the "OLS" functionality from SOLR</li>
+     * </ol>
+     * 
+     * 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        // set response type to JS and allow programs from other servers to send and receive
+        response.setContentType("application/json;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        boolean test = false; // still alive test
+        boolean error = false; // has an error been detected?
+        String solrResult = ""; // JSON doc sent back to UI
+
+        // create URL for SOLR query
+        String queryURL = url;
+        boolean first = true;
+        Map<String, String[]> params = request.getParameterMap(); // get map of parameters and their values
+        Enumeration<String> allParams = request.getParameterNames(); // get a list of parameter names
+        while (allParams.hasMoreElements()) {
+            String param = allParams.nextElement();
+            if (param.equalsIgnoreCase("phenotype")) { // deal with phenotypes
+                if (!first) { // if this is not the first parameter added to queryURL include separator
+                    queryURL += "&";
+                }
+                queryURL += "phenotype=" + params.get("phenotype")[0]; // extend stem with parameter
+                first = false; // next time you need a separator
+            } else if (param.equalsIgnoreCase("ptId")) { // deal with phenotypes
+                if (!first) { // if this is not the first parameter added to queryURL include separator
+                    queryURL += "&";
+                }
+                queryURL += "phenotype=" + params.get("ptId")[0]; // extend stem with parameter
+                first = false; // next time you need a separator                
+            } else if (param.equalsIgnoreCase("anatomy")) { // deal with anatomy 
+                if(!first) {
+                    queryURL += "&";
+                }
+                queryURL += "anatomy="+params.get("anatomy")[0];
+                first = false;
+            } else if (param.equalsIgnoreCase("aId")) { // deal with anatomy 
+                if(!first) {
+                    queryURL += "&";
+                }
+                queryURL += "anatomy="+params.get("aId")[0];
+                first = false;                
+            } else if (param.equalsIgnoreCase("gene")) { // deal with genes
+                if(!first) {
+                    queryURL += "&"; 
+                }
+                queryURL += "gene="+params.get("gene")[0];
+                first = false;
+            } else if (param.equalsIgnoreCase("gId")) { // deal with genes
+                if(!first) {
+                    queryURL += "&"; 
+                }
+                queryURL += "gene="+params.get("gId")[0];
+                first = false;                
+            } else if (param.equalsIgnoreCase("start")) { // number of initial result
+            	if(!first) {
+            		queryURL += "&";
+            	}    
+            	queryURL += "start="+params.get("start")[0];
+            	first = false;
+            } else if (param.equalsIgnoreCase("num")) { // number of results to return
+            	if(!first) {
+            		queryURL += "&";
+            	}
+            	queryURL += "resultNo="+params.get("num")[0];
+            } else if(param.equalsIgnoreCase("alive")) {
+                test = true;
+                solrResult = "{\"alive\" : \"true\"}";                
+            } else { // parameter was not recognised, send error
+                error = true; // error has been detected
+                logger.log(Level.WARNING, "Client sent invalid parameter: "+param);
+                solrResult = "{\"invalid_paramater\": \"" + param + "\"}";
+            }
+        }
+        
+        // should write query to log?                
+
+        // run solr query
+        if (!error && !test) { // if no error detected && not a test
+            BufferedReader in = null;
+            try {
+                // connect to SOLR and run query
+                URL url = new URL(queryURL);
+                in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                // read JSON result
+                String inputLine;
+                if ((inputLine = in.readLine()) != null) { // should only be 1 line of result
+                    // can currently return the API result unchanged
+		    		solrResult = inputLine;
+                }
+            } catch (IOException e) {
+                logger.log(Level.WARNING, e.getMessage());
+                solrResult = "{\"server_error\": \""+e.getMessage()+"\"}"; 
+            }
+        }
+
+        // send result to client (UI)
+        PrintWriter out = response.getWriter();
+        try {
+            out.println(solrResult); // may be error or genuine result
+        } finally {
+            out.close();            
+        }
+    }
+
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Simple service that wraps the SOLR API to enable searching of image information";
+    }// </editor-fold>
+
+}
+
---
+++ HWU/src/main/webapp/index.jsp
@@ -0,0 +1,61 @@
+<html>
+<body>
+<h2>IQS API summary</h2>
+<div>
+Base URL is http://lxbisel.macs.hw.ac.uk:8080/IQS/
+</div>
+<div>
+<h3>/getimages</h3>
+<h4>parameters</h4>
+All parameters are optional
+<ol>
+<li>phenotype - phenotype ID or term, e.g. MP:0010254 or cataracts</li>
+<li>anatomy - anatomy ID or term, e.g. MA:0000261 or eye</li>
+<li>gene - gene ID or symbol, e.g. MGI:1891295 or Spns2</li>
+<li>num - number of results to be returned, default is 100</li>
+<li>start - first result to be returned, default is 0</li>
+</ol>
+
+Examples:
+<ol>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getimages?phenotype=MP:0010254</li>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getimages?phenotype=MP:0010254&start=1</li>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getimages?phenotype=MP:0010254&num=1</li>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getimages?phenotype=cataracts</li>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getimages?anatomy=eye</li>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getimages?gene=Spns2</li>
+
+</ol>
+</div>
+
+<div>
+<h3>/getroi</h3>
+<h4>parameters</h4>
+Parameter is mandatory
+<ol>
+<li>id - ROI ID, e.g. komp2_roi_112003_0</li>
+</ol>
+
+Example:
+<ol>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getroi?id=komp2_roi_112003_0</li>
+</ol>
+</div>
+
+<div>
+<h3>/getchannel</h3>
+Parameter is mandatory
+<h4>parameters</h4>
+<ol>
+<li>id - Channel ID, e.g., komp2_channel_112003_0</li>
+</ol>
+
+Example:
+<ol>
+<li>http://lxbisel.macs.hw.ac.uk:8080/IQS/getchannel?id=komp2_channel_112003_0</li>
+</ol>
+</div>
+
+</div>
+</body>
+</html>
