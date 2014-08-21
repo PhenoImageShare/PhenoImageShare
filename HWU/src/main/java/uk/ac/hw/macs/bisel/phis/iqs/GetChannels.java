0044df4b29392b53c213c47b93b2ad0478a4ae3a
Kenneth McLeod <kcm1@hw.ac.uk>
2014-08-21 16:26:45 +0100

    update to align with latest beta version of api (add pagination)

--- HWU/src/main/java/uk/ac/hw/macs/bisel/phis/iqs/GetChannels.java
+++ HWU/src/main/java/uk/ac/hw/macs/bisel/phis/iqs/GetChannels.java
@@ -12,50 +12,50 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * Servlet connects client (presumably a GUI) to the SOLR API that wraps the SOLR repository
- * for image channels
+ * Servlet connects client (presumably a GUI) to the SOLR API that wraps the
+ * SOLR repository for image channels
  *
  * @author kcm
  */
-//@WebServlet(name = "GetChannels", urlPatterns = {"/GetChannels"})
 public class GetChannels extends HttpServlet {
 
-    private static final String url = "http://dev.phenoimageshare.org/data/rest/getChannels?"; // stem of every SOLR query
+    private static final String url = "http://beta.phenoimageshare.org/data/rest/getChannels?"; // stem of every SOLR query
     private static final Logger logger = Logger.getLogger(System.class.getName());
+
     /**
-     * Enables discovery of channel information for a single channel.
+     * Enables discovery of channel information for multiple channels.
      *
-     * Handles requests from the PhIS UI by simply forwarding them to the SOLR API and 
-     * then returning the result directly to the UI.  Provides very basic error handling
-     * (only deals with unknown parameters).
+     * Handles requests from the PhIS UI by simply forwarding them to the SOLR
+     * API and then returning the result directly to the UI. Provides very basic
+     * error handling (only deals with unknown parameters).
      *
-     * 
-     * This has no need for pagination as only 1 channel can be returned per query
      *
      * NOTE: this communicates with SOLR API not the SOLR core directly
-     * 
+     *
      * Parameters expected:
      * <ol>
-     * <li>id = channel ID, e.g., komp2_channel_112003_0</li>
+     * <li>imageId = image ID, e.g., komp2_112003</li>
+     * <li>start = pagination, the first result that should be returned</li>
+     * <li>num = pagination, the total number of results that should be
+     * returned</li>
      * </ol>
-     * 
+     *
      * Future versions will:
      * <ol>
-     * <li>send queries to the SIS, and then integrate the results
-     * with those from SOLR</li>  
+     * <li>send queries to the SIS, and then integrate the results with those
+     * from SOLR</li>
      * <li>likely to include sorting the results</li>
      * <li>include a wider range of query parameters</li>
      * <li>provide access to the "OLS" functionality from SOLR</li>
      * </ol>
-     * 
-     * 
+     *
+     *
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
@@ -66,7 +66,7 @@ public class GetChannels extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
+
         // set response type to JS and allow programs from other servers to send and receive
         response.setContentType("application/json;charset=UTF-8");
         response.setHeader("Access-Control-Allow-Origin", "*");
@@ -77,26 +77,40 @@ public class GetChannels extends HttpServlet {
         // create URL for SOLR query
         String queryURL = url;
         boolean first = true;
-		Map<String, String[]> params = request.getParameterMap(); // get map of parameters and their values
+        Map<String, String[]> params = request.getParameterMap(); // get map of parameters and their values
         Enumeration<String> allParams = request.getParameterNames(); // get a list of parameter names
         if (allParams.hasMoreElements()) {
             String param = allParams.nextElement();
-            if (param.equalsIgnoreCase("id")) { // ID of channel
+            if (param.equalsIgnoreCase("imageId")) { // ID of channel
                 if (!first) { // at the moment it will always be the first (and only) param
                     queryURL += "&";
                 }
-                
-                queryURL += "channelId=" + params.get("id")[0]; // extend stem with parameter
+
+                queryURL += "imageId=" + params.get("id")[0]; // extend stem with parameter
                 first = false; // next time you need a separator
+
+            // pagination    
+            } else if (param.equalsIgnoreCase("start")) { // number of initial result
+                if (!first) {
+                    queryURL += "&";
+                }
+                queryURL += "start=" + params.get("start")[0];
+                first = false;
+            } else if (param.equalsIgnoreCase("num")) { // number of results to return
+                if (!first) {
+                    queryURL += "&";
+                }
+                queryURL += "resultNo=" + params.get("num")[0];
+
+            // error handling    
             } else { // parameter was not recognised, send error
                 error = true; // error has been detected
-                logger.log(Level.WARNING, "Client sent invalid parameter: "+param);
+                logger.log(Level.WARNING, "Client sent invalid parameter: " + param);
                 solrResult = "{\"invalid_paramater\": \"" + param + "\"}";
             }
         }
-        
-        // should write query to log?
 
+        // should write query to log?
         // run query against SOLR API
         if (!error) { // if no error detected
             BufferedReader in = null;
@@ -108,12 +122,12 @@ public class GetChannels extends HttpServlet {
                 // read JSON result
                 String inputLine;
                 if ((inputLine = in.readLine()) != null) { // should only be 1 line of result
-                	// send result directly to client, should be no need to process (at the moment)
-				    solrResult = inputLine;
+                    // send result directly to client, should be no need to process (at the moment)
+                    solrResult = inputLine;
                 }
             } catch (IOException e) {
                 logger.log(Level.WARNING, e.getMessage());
-                solrResult = "{\"server_error\": \""+e.getMessage()+"\"}"; 
+                solrResult = "{\"server_error\": \"" + e.getMessage() + "\"}";
             }
         }
 
@@ -122,10 +136,9 @@ public class GetChannels extends HttpServlet {
         try {
             out.println(solrResult); // may be error or genuine result
         } finally {
-            out.close();            
+            out.close();
         }
     }
-
 
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -166,4 +179,3 @@ public class GetChannels extends HttpServlet {
     }// </editor-fold>
 
 }
-
