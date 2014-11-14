59538fc24b5609118f49889c7900a6ded5c8adae
David Osumi-Sutherland <dosumis@gmail.com>
2014-11-14 23:20:39 +0000

    working on final bits of VFB2PhiSXML interface and runner.  Not left in a working state - but getting there.

--- VFB_import/src/VFB2PhisXML.py
+++ VFB_import/src/VFB2PhisXML.py
@@ -20,21 +20,26 @@ import warnings
 
 # Generator functions live outside the classes. They generate objects that must then be bound.
 
-def gen_OntologyTerm(ont, ID):
-    # ont is an id/name lookup for ontology terms used here.  Populate with separate script calling brain
+def gen_OntologyTerm(id_name, ID):
+    """Takes id_name lookup dict for ontology terms and an ID
+    Returns a phisSchema.OntologyTerm object"""
     ot = phisSchema.OntologyTerm()
     ot.termId = ID
-    ot.termLabel = ont[ID]
+    ot.termLabel = id_name[ID]
     return ot
 
 def gen_Link(display_name, url):
-    """Takes display_name and URI as args and returns a gen_Link"""
+    """Takes display_name and URI as args and returns a phisSchema.Link object"""
     gen_Link = phisSchema.Link()
     gen_Link.display_name = display_name
     gen_Link.url = url
     return gen_Link
 
 def gen_Annotation(ot, text, mode):
+    """Generate a phisSchema.Annotation object based on specified:
+    ot: ontology term
+    text: free text
+    mode:  Manual/Automated"""
     annotation = phisSchema.Annotation()
     annotation.annotation_freetext = text
     annotation.ontology_term = ot
@@ -42,8 +47,10 @@ def gen_Annotation(ot, text, mode):
     return annotation
 
 def gen_roi_Coordinates(x, y, z):
-    """Generate a set of coordinates for an roi - expressed as percentage ranges
-     in the form of a list or tuple with 2 elements"""
+    """Generate a phisSchema.Coordinates object for an roi 
+    Each arg specifies a range in the form of a list or tuple 
+    with 2 elements
+    """
     try:
         assert len(x) == 2
         assert len(y) == 2
@@ -52,20 +59,22 @@ def gen_roi_Coordinates(x, y, z):
         warnings.warn("Percent arrays should have only 2 members - specifying a range.")
         
     coord = phisSchema.Coordinates()
-    coord.x_coordinates = gen_PercentArray(*x)
-    coord.y_coordinates = gen_PercentArray(*y)
-    coord.z_coordinates = gen_PercentArray(*z)
+    coord.x_coordinates = _gen_PercentArray(*x)
+    coord.y_coordinates = _gen_PercentArray(*y)
+    coord.z_coordinates = _gen_PercentArray(*z)
     return coord
 
-def gen_PercentArray(a, b):
+def _gen_PercentArray(a, b):
     AB = (a, b)
     pa = phisSchema.PercentArray()
     pa.extend(AB)
     return pa
 
-def gen_GenotypeComponent(gf_symbol, gf_id, gene_symbol, gene_id, gf_ensembl_id):
+def gen_GenotypeComponent(gf_symbol=False, gf_id=False, gene_symbol=False, gene_id=False, gf_ensembl_id=False):
     ## How to specify channel.  Use defaults? ###
-    """All args are strings. If arg is not used, please set to False."""
+    """Generate a phisSchema.GenotypeComponent object. 
+    All args are strings. Please specify each arg with a keyword
+    """
     gc = phisSchema.GenotypeComponent()
     if gene_id:
         gc.gene_id = gene_id
@@ -79,24 +88,58 @@ def gen_GenotypeComponent(gf_symbol, gf_id, gene_symbol, gene_id, gf_ensembl_id)
         gc.genetic_feature_ensembl_id = gf_ensembl_id
     return gc
 
-class VFB_image_dataset():
-    """Class to use for generating sets of images from a common source."""
+class imageDataSet():
+    """Class to use for generating sets of images from a common source.
+    Assumes all datasets have common source name and URL. 
+    And that they share a background channel marker and visualization methods 
+    for background and signal channels.  All of these are set by methods rather than KWARGS.
+    """
     # May not be worth bothering with a class here
-    def __init__(self, source):
-        """Source should be a phisSchema.Link object.""" 
+    def __init__(self, ont_dict):
         ### Do we have a way to distinguish general source from specific source links?
         self.doc = phisSchema.Doc() 
-        self.source = source
-
-class VFB_image():
+        self.source = ''
+        self.background_channel_marker = ''
+        self.signal_channel_visualisation_methods = []
+        self.background_channel_visualisation_methods = []
+        
+    def set_source(self, source_name, source_url):
+        """source_name and source_url are strings"""
+        self.source = gen_Link(source_name, source_url)
+        
+    def set_background_channel_marker(self, genotype_component):
+        """Takes a phisSchema.genotypeComponent object as an arg"""
+        self.background_channel_marker = genotype_component
+        
+    def add_signal_channel_visualisation_method(self, sfid):
+        """sfid is the shortFormId of and FBbi visualisation method"""
+        self.signal_channel_visualisation_methods.append(gen_OntologyTerm(self.ont_dict, sfid))
+        
+    def add_background_channel_visualisation_method(self, sfid):
+        """sfid is the shortFormId of and FBbi visualisation method"""
+        self.background_channel_visualisation_methods.append(gen_OntologyTerm(self.ont_dict, sfid))
+        
+    
+class VfbImage():
     """Interface class for loading VFB data.
     Assumes 3D confocal image with 2 channels - 
-    a background stain channel depicting adult brain and 
-    a signal channel depicting some interesting expression/anatomy"""
+    a background stain channel and  a signal channel 
+    depicting some interesting expression/anatomy"""
     # Define constants here:  Or should this just jump straight to populating model?
     host = gen_Link("Virtual Fly Brain", "http://www.virtualflybrain.org")  # for image_description.host
-    def __init__(self, ont):
+    def __init__(self, ont, image_dataset):
+        """ont: an ID:name dict of ontology terms used in XML to be produced
+            d: A image_dataset object
+        """
         self.ont = ont
+        self._initialise_image()
+        self._unpack_image_dataset(image_dataset)
+        
+    def _unpack_image_dataset(self, image_dataset):
+        self.set_source(image_dataset.source)
+        self.set_signal_channel_visualisation_method(image_dataset.)  # Needs extend rather than append?
+        self.set_background_channel_visualisation_method(image_dataset.) # Needs extend rather than append?
+        self.set_expressed_feature_for_background_channel(image_dataset.background_channel_marker)
         
     def set_organism(self, stage, sex):
         """stage must be a phisSchema.ontologyTerm object; sex must be the string 'Male' or 'Female'"""
@@ -107,13 +150,12 @@ class VFB_image():
         organism.stage=stage
         self.image.organism = organism
         
-
-    
     def _initialise_image(self):
         """Assume 2 channels each with an associated ROI at 100%.
         All objects generated by multiple iterations appended to common doc.
         Generate IDs for two channels and corresponding ROIs according to the scheme:
-        image_id-a/b roi_id-a/b; channel_id-a/b - where id = self.vfb_image_id."""
+        image_id-a/b roi_id-a/b; channel_id-a/b - where id = self.vfb_image_id.
+        channel1/roi1 = background.  channel2/roi2 = signal."""
         
         # Generate Root objects
         self.image = phisSchema.Image()
@@ -208,10 +250,13 @@ class VFB_image():
         self.image.image_description.image_generated_by = source
         self.image.image_description.organism_generated_by = source
     
-    def set_visualisation_method(self, sfid):
-        #Should request again that visualisation method be moved down to channel
-        self.image.image_description.visualisation_method =  pyxb.BIND() # FBbi_00000156"
-        self.image.image_description.visualisation_method.append(gen_OntologyTerm(self.ont, sfid)) 
+    def set_background_channel_visualisation_method(self, sfid):
+        self.channel2.visualisation_method =  pyxb.BIND() 
+        self.channel2.visualisation_method.append(gen_OntologyTerm(self.ont, sfid)) 
+        
+    def set_signal_channel_visualisation_method(self, sfid):
+        self.channel2.visualisation_method =  pyxb.BIND() 
+        self.channel2.visualisation_method.append(gen_OntologyTerm(self.ont, sfid))
         
     def add_background_depicted_entity(self, sfid, text, mode):
         # By convention, background channel is always roi1
@@ -231,31 +276,48 @@ class VFB_image():
         else:
             self.channel2.is_expression_pattern = "No"
 
-                           
+    def set_expressed_feature_for_signal_channel(self, genotype_component):
+        """genotype_component: a phisSchema.GenotypeComponent object."""
+        self.channel2.depicts_expression_of = genotype_component
+        
+    def set_expressed_feature_for_background_channel(self, genotype_component):
+        """genotype_component: a phisSchema.GenotypeComponent object."""
+        self.channel1.depicts_expression_of = genotype_component
+    
  
         # Bind to appropriate channel in main code?
         
 
-class VFB_wt_adult_brain_image(VFB_image):
-    """Assume wt adult adult brain image. Dimensions = 512,512,512"""
+class VfbWtAdultBrainImage(VfbImage):
     """Args:
-     ont is a name_id dict lookup for ontology terms. 
-    doc is a phisSchema.Doc instance
-    vfb_image_id is an id string for the image
-    image_url is also a string
-    Compulsory fields to set in order to generate XML
-    set_sex("Male/Female"
-    set_source(Link)
-    set_is_expression_pattern(True/False)
-    add_depicted_anatomy_for_expressed_feature()
+       - ont is a name_id dict lookup for ontology terms. 
+       - image_dataset is an imageDataSet object
+       - vfb_image_id is an id string for the image
+       - image_url is also a string
+    Compulsory fields to set in order to generate XML:
+       - set_sex("Male/Female")
+       - set_is_expression_pattern(True/False)
+       - add_depicted_anatomy_for_expressed_feature(ont_term)
+    Other necessary fields to set for usable XML:
+       - set_expressed_feature
+       - set_visualisation_method
+    Set by default:
+       - sample prep: chemically fixed; whole mount tissue
+       - imaging methods: confocal microscopy
+       - image has 2 channels - one background, and one signal.
+       - organism: Dmel
+       - stage: adult
+       - Background channel anatomy: adult brain
+       - Dimensions = 512,512,512
     """
 
-    
     # Consider ditching this subclass if don't find a bunch of more specific things to say.  Might be better to have subclasses for neuron, clone and expression pattern
-# One doc for all images.
-    def __init__(self, ont, doc, vfb_image_id, image_url):
-        self.doc = doc
+    # One doc for all images.
+    def __init__(self, ont, image_dataset, vfb_image_id, image_url):
+        
         self.ont = ont
+        self.doc = image_dataset.doc
+        self.set_source(image_dataset.source)
         self.stage = gen_OntologyTerm(ont, "FBdv_00005369")  # Hmmmm - global!
         self.vfb_image_id = vfb_image_id
         self._initialise_image()
--- VFB_import/src/pdm2xml_runner.py
+++ VFB_import/src/pdm2xml_runner.py
@@ -9,28 +9,59 @@ import phisSchema
 
 # For individual in pdm
 
-lookup = ''
-pdm = ''
 
-d = phisSchema.Doc()
+def load_json(path):
+    json_file = open(path, "r")
+    json_string = json_file.read()
+    json_file.close()
+    return json.loads(json_string)
+
+
+ont_dict = load_json('')  # Load as json dump, produced by Brain queries over merged onts
+
+pdm = load_json("/repos/VFB_owl/src/code/export/Cachero2010.json")
+
+source_name = 'Cachero2010'
+source_link = '' # Link to FlyBase pub page
+image_set = VP.imageDataSet(source_name, source_link)
+# TO add to VP
+
+x= VP.VfbWtAdultBrainImage(ont_dict, image_dataset)
+
+x.
+
+x.add_background_depicted_entity(sfid, text, mode)
+x.add_depicted_anatomy_for_expressed_feature(sfid, text, mode)
+x.set_is_expression_pattern(s)
+x.set_sex(sex)
+x.set_source(source)
+x.
+### 
 
 
 for i, v in pdm.items():
     image_url = ''  ### Where is this coming from?
     
-    out = VP.VFB_wt_adult_brain_image(lookup, d, i, image_url)
+    out = VP.VfbWtAdultBrainImage(ont_dict, image_set, i, image_url)
     out.set_source(VP.gen_Link("FlyCircuit", "http://flycircuit.net"))
     out.set_is_expression_pattern(False) # Set as false unless types as expression pattern 
     for t in v["Types"]:
-        if t["is_anonymous"] == True:
+        if not t["is_anonymous"]:
             if t["object_id"] == "http://purl.obolibrary.org/obo/vfb/B8C6934B-C27C-4528-BE59-E75F5B9F61B6" :
                 out.set_is_expression_pattern(True)
-            elif t["object"]: == ""
-        if t["rel_id"] == "":
-            out.add_depicted_anatomy_for_expressed_feature(, "stuff", "Manual")
-            out.set_sex("Male")
+            else:
+                out.add_depicted_anatomy_for_expressed_feature(t["object_id"], "", "Manual")
+        else:
+            #overlaps:
+            if t["rel_id"] == "RO_0002131":
+                out.add_depicted_anatomy_for_expressed_feature(t["object_id"], "", "Manual")  # But better if could check for subproperties of overlaps 
+            #expresses
+            if t["rel_id"] == "RO_":
+                # set expressed feature to t["object_if"]
+                stub = 1
 
 
-outfile = open("", "")
+
+outfile = open("tmp.xml", "w")
 outfile.write(d.toxml())
 
