3eb37ab8208da0bd152318fe7f0043b24f1cecfd
Ilinca Tudose <ilinca.tudose@gmail.com>
2015-02-18 11:05:25 +0000

    Removed facet.mincount = 1

--- PhIS/source/main/java/uk/ac/ebi/phis/service/ImageService.java
+++ PhIS/source/main/java/uk/ac/ebi/phis/service/ImageService.java
@@ -227,7 +227,6 @@ public class ImageService {
 		solrQuery.addFacetField(ImageDTO.IMAGING_METHOD_LABEL);
 		solrQuery.addFacetField(ImageDTO.TAXON);
 		solrQuery.addFacetField(ImageDTO.SAMPLE_TYPE);
-		solrQuery.setFacetMinCount(1);
 		
 		// add pivot facets to get the number of image types per 
 		solrQuery.set("facet.pivot", ImageDTO.SAMPLE_TYPE + "," + ImageDTO.IMAGE_TYPE);	
@@ -412,5 +411,4 @@ public class ImageService {
 	public String getQueryUrl(SolrQuery q){
 		return solr.getBaseURL() + "/select?" + q.toString();
 	}
-	
 }
