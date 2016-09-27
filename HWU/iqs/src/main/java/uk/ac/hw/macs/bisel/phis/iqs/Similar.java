8f3d6de83193795ec54ee060062762c25cb10243
Kenneth McLeod <mcleod.kc@gmail.com>
2016-09-27 09:55:21 +0100

    iqs v103

---
+++ HWU/iqs/src/main/java/uk/ac/hw/macs/bisel/phis/iqs/Similar.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2015 BISEL, Heriot-Watt University, Edinburgh, UK (http://www.bisel.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.hw.macs.bisel.phis.iqs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kcm
+ */
+@WebServlet(name = "Similar", urlPatterns = {"/similar"})
+public class Similar extends HttpServlet {
+
+    private static final Logger logger = Logger.getLogger(System.class.getName());
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        logger.log(Level.INFO, "[GUI QUERY] {0}", request.getRequestURI() + "?" + request.getQueryString());
+
+        // check to find version and forward
+        Map<String, String[]> params = request.getParameterMap(); // get map of parameters and their values
+        String[] versions = params.get("version");
+
+        if (versions == null) {
+            // default is v103
+            request.getRequestDispatcher("/v103S").forward(request, response);
+        } else if (versions[0].equals("103")) {
+            request.getRequestDispatcher("/v103S").forward(request, response);
+        } else {
+            // otherwise forward to default
+            request.getRequestDispatcher("/v103S").forward(request, response);
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
